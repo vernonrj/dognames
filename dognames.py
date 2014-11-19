a932bdde9d1aa689f3a6e501dc97f9f3f756f5b1
Vernon Jones <vernonrjones@gmail.com>
2014-11-18 22:27:20 -0800

    Some small tweaks

--- dognames.py
+++ dognames.py
@@ -5,6 +5,18 @@ from functools import reduce
 from dog_list import DOGS
 
 
+def word_iter(words):
+    """iterate on words"""
+    word = []
+    for char in words:
+        if char in ' \n\t':
+            if word:
+                yield ''.join(word)
+                word = []
+        else:
+            word.append(char)
+
+
 def rand_elem(indexable):
     """return an element at random from an indexable collection"""
     elem = random.randint(0, len(indexable) - 1)
@@ -12,10 +24,7 @@ def rand_elem(indexable):
 
 def hyphenate(phrase):
     """hyphenate a phrase correctly"""
-    try:
-        return reduce(list.__add__, (x.split('-') for x in phrase.split()))
-    except IndexError:
-        return []
+    return reduce(list.__add__, (x.split('-') for x in phrase.split()))
 
 def mangle_dog_names(names):
     """return all those names mangled up"""
